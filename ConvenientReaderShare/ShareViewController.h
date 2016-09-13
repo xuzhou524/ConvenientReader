9beb6f2b2fb56e62fa92fa520a03b9662028e9ea
xuzhou524 <xuuohz@163.com>
2016-09-13 17:02:10 +0800

    bak

---
+++ ConvenientReaderShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ConvenientReaderShare
+//
+//  Created by gozap on 16/9/13.
+//  Copyright © 2016年 xuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
