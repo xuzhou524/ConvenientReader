d973e10506e591b7cc539e1a8e595583cdcb22c5
xuzhou524 <xuuohz@163.com>
2016-09-18 17:48:17 +0800

    bak

--- ConvenientReaderShare/ShareViewController.m
+++ ConvenientReaderShare/ShareViewController.m
@@ -9,26 +9,64 @@
 #import "ShareViewController.h"
 
 @interface ShareViewController ()
-
+@property(nonatomic,strong)NSString * urlString;
 @end
 
 @implementation ShareViewController
 
+- (void)viewDidLoad{
+    NSExtensionItem * imageItem = [self.extensionContext.inputItems firstObject];
+    NSItemProvider * imageItemProvider = [[imageItem attachments] firstObject];
+    if([imageItemProvider hasItemConformingToTypeIdentifier:@"com.adobe.pdf"]){
+        NSLog(@"xxxxxxxx");
+        [imageItemProvider loadItemForTypeIdentifier:@"com.adobe.pdf" options:nil completionHandler:^(NSURL* imageUrl, NSError *error) {
+            //在这儿做自己的工作
+            NSLog(@"xxxxxxx123 = %@",imageUrl.absoluteString);
+            _urlString = imageUrl.absoluteString;
+        }];
+    }
+}
 - (BOOL)isContentValid {
-    // Do validation of contentText and/or NSExtensionContext attachments here
+    NSExtensionItem * imageItem = [self.extensionContext.inputItems firstObject];
+    if(!imageItem){
+        return NO;
+    }
+
+    NSItemProvider * imageItemProvider = [[imageItem attachments] firstObject];
+    if(!imageItemProvider){
+        return NO;
+    }
+
+    if([imageItemProvider hasItemConformingToTypeIdentifier:@"public.url"]&&self.contentText){
+        return YES;
+    }
     return YES;
 }
 
 - (void)didSelectPost {
-    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+    NSExtensionItem * imageItem = [self.extensionContext.inputItems firstObject];
+    //完成一些自己的操作 保存，添加 http请求
+    UIAlertController * alter = [UIAlertController alertControllerWithTitle:@"11" message:@"111" preferredStyle:UIAlertControllerStyleAlert];
+    [self presentViewController:alter animated:YES completion:^{
+    }];
+    NSExtensionItem * outputItem = [imageItem copy];
+
+    outputItem.attributedContentText = [[NSAttributedString alloc] initWithString:self.contentText attributes:nil];
+    NSArray * outPutitems= @[outputItem];
+    [self.extensionContext completeRequestReturningItems:outPutitems completionHandler:nil];
 }
 
 - (NSArray *)configurationItems {
-    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-    return @[];
+    SLComposeSheetConfigurationItem * oneItem = [[SLComposeSheetConfigurationItem alloc]init];
+  
+    oneItem.title = @"123";
+ 
+    oneItem.valuePending = NO;
+    
+    oneItem.tapHandler = ^(void){
+        NSLog(@"dsdsdsdsd");
+    };
+    return @[oneItem];
 }
 
 @end
